9d4897e86d2107db86b3ce7e976499339e4d1ba1
maminjie <ma_minjie@hoperun.com>
2019-09-18 09:52:02 +0800

    change:wakaama, rename the dir name wakaama to wakaama-master

--- iot_link/network/lwm2m/lwm2m_imp/wakaama/wakaama.mk
+++ iot_link/network/lwm2m/lwm2m_imp/wakaama/wakaama.mk
@@ -1,21 +1,24 @@
 ################################################################################
 # this is used for compile the wakaama
 ################################################################################
+WAKAAMA_DIR = $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama
+WAKAAMA_BASE_DIR = $(WAKAAMA_DIR)/wakaama-master
+
 WAKAAMA_COAP_SRC = \
-        ${wildcard $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/core/er-coap-13/er-coap-13.c}
+        ${wildcard $(WAKAAMA_BASE_DIR)/core/er-coap-13/er-coap-13.c}
         C_SOURCES += $(WAKAAMA_COAP_SRC)
 
 WAKAMA_LWM2MSRC = \
-        ${wildcard $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/core/*.c} \
-        ${wildcard $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/examples/shared/*.c}
+        ${wildcard $(WAKAAMA_BASE_DIR)/core/*.c} \
+        ${wildcard $(WAKAAMA_BASE_DIR)/examples/shared/*.c}
         C_SOURCES += $(WAKAMA_LWM2MSRC)
         
 WAKAAMA_COAP_INC = \
-        -I $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/core/er-coap-13
+        -I $(WAKAAMA_BASE_DIR)/core/er-coap-13
         C_INCLUDES += $(WAKAAMA_COAP_INC)
 WAKAMA_LWM2MINC = \
-        -I $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/core \
-        -I $(iot_link_root)/network/lwm2m/lwm2m_imp/wakaama/examples/shared
+        -I $(WAKAAMA_BASE_DIR)/core \
+        -I $(WAKAAMA_BASE_DIR)/examples/shared
         C_INCLUDES += $(WAKAMA_LWM2MINC)        
        
 C_DEFS += -D LWM2M_LITTLE_ENDIAN -D LWM2M_CLIENT_MODE -D LWM2M_WITH_LOGS
